cd9f1eb46879c26c2ea2e7224f3556f820b6effe
Haritha <b.reddy10@gmail.com>
2021-07-27 07:24:44 +0000

    intial commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_instance" "ganesh" {
+  ami = "ami-0603cbe34fd08cb81"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Ganesh"
+    Env = "prod"
+  }
+}
+
+output "myawsserver-ip" {
+  value = aws_instance.ganesh.public_ip
+}
+
+output "myserver-PrivateIP" {
+  value = aws_instance.ganesh.private_ip
+}
